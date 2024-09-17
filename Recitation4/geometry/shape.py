a91789897cded3ff52cc014803e78f33af024d1f
sandrawiktor <swiktor@uncc.edu>
2024-09-17 17:32:35 -0400

    Added abstractmethod indicator to methods in shape

--- Recitation4/geometry/shape.py
+++ Recitation4/geometry/shape.py
@@ -4,17 +4,21 @@ class Shape(ABC):
 
     def __init__(self, shape):
         self.shape = shape
-        pass
 
+    @abstractmethod
     def find_area(self):
+        """Method to compute the area of the shape."""
         pass
 
+    @abstractmethod
     def find_perimeter(self):
+        """Method to compute the perimeter of the shape."""
         pass
 
+    @abstractmethod
     def description(self):
+        """Method to provide a description of the shape."""
         pass
 
     def info(self):
         return f"The shape is a {self.shape}."
-
